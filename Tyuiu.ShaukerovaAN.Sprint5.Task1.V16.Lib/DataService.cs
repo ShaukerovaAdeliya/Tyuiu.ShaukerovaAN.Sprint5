174d6c31db755d0590f160eef75112afce26365d
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-11-19 18:18:03 +0500

    Task1

---
+++ Tyuiu.ShaukerovaAN.Sprint5.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ShaukerovaAN.Sprint5.Task1.V16.Lib
+{
+    public class DataService : ISprint5Task1V16
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsists = fileInfo.Exists;
+
+            if (fileExsists)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Sin(x) + (2 * x / 3) - Math.Cos(x) * 4 * x, 2);
+                strY = Convert.ToString(y);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ShaukerovaAN.Sprint5.Task1.V16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaukerovaAN.Sprint5.Task1.V16.Lib;
+
+namespace Tyuiu.ShaukerovaAN.Sprint5.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#5                                                                *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение и сохраняет             *");
+            Console.WriteLine("* в текстовый файл OutPutFileTask1.txt                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
