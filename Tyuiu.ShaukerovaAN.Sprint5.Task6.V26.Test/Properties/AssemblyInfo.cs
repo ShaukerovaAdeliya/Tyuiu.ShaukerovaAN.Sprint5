5ccc788b74c29475f1329d9946ef0c4a66fbe9ff
ShaukerovaAdeliya <Tyuiu.ShaukerovaAN@outlook.com>
2023-11-26 21:55:13 +0500

    Task6

---
+++ Tyuiu.ShaukerovaAN.Sprint5.Task6.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShaukerovaAN.Sprint5.Task6.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShaukerovaAN.Sprint5.Task6.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2074e76c-6ab0-49b0-b5dd-ff146e1985e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShaukerovaAN.Sprint5.Task6.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.ShaukerovaAN.Sprint5.Task6.V26.Lib;
+
+namespace Tyuiu.ShaukerovaAN.Sprint5.Task6.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Шаукерова А.Н. | ИСТНб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Обработка текстовых файлов                                              *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнила: Шаукерова Аделия Нуржановна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask6V26.txt в котором есть набор  *");
+            Console.WriteLine("* символьных данных.Найти количество знаков вопроса в заданной строке.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            double symbol = ds.LoadFromDataFile(path);
+            Console.WriteLine("количество знаков вопроса в заданной строке равно " + symbol);
+            Console.ReadLine();
+        }
+    }
+}
